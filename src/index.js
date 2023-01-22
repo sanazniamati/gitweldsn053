69c48ddd0f9dfd4cfb6dc08c736baeb2c0dc8d43
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-01-22 17:32:01 +0330

    add input  change strokeWidth of line

--- src/index.js
+++ src/index.js
@@ -1,59 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import { Stage, Layer, Line, Text } from "react-konva";
+import { Stage, Layer, Line, Text, Group } from "react-konva";
 
 const App = () => {
+  const [strokeWidth, setStrokeWidth] = useState(3);
+  const handelOnChange = (e) => {
+    setStrokeWidth(e.target.value);
+  };
   return (
     <>
+      <input type={"number"} value={strokeWidth} onChange={handelOnChange} />
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
           <Line
             x={10}
+            y={7}
             stroke={"green"}
-            strokeWidth={3}
-            points={[100, 10, 300, 10]}
+            strokeWidth={strokeWidth}
+            points={[300, 0, 600, 0]}
           />
-          <Text x={350} y={2} text={"strokeWidth : 3"} fontSize={20} />
+          <Text
+            x={650}
+            y={-1}
+            text={`strokeWidth : ${strokeWidth}`}
+            fontSize={20}
+          />
+          <Group y={50}>
+            <Line
+              x={10}
+              stroke={"green"}
+              strokeWidth={3}
+              points={[100, 10, 300, 10]}
+            />
+            <Text x={350} y={2} text={`strokeWidth : 3`} fontSize={20} />
+            <Line
+              x={10}
+              stroke={"green"}
+              strokeWidth={1.5}
+              points={[100, 40, 300, 40]}
+            />
+            <Text x={350} y={30} text={"strokeWidth :1.5"} fontSize={20} />
 
-          <Line
-            x={10}
-            stroke={"green"}
-            strokeWidth={1.5}
-            points={[100, 40, 300, 40]}
-          />
-          <Text x={350} y={30} text={"strokeWidth :1.5"} fontSize={20} />
+            <Line
+              x={10}
+              stroke={"green"}
+              strokeWidth={1.2}
+              points={[100, 70, 300, 70]}
+            />
+            <Text x={350} y={60} text={"strokeWidth :1.2"} fontSize={20} />
 
-          <Line
-            x={10}
-            stroke={"green"}
-            strokeWidth={1.2}
-            points={[100, 70, 300, 70]}
-          />
-          <Text x={350} y={60} text={"strokeWidth :1.2"} fontSize={20} />
+            <Line
+              x={10}
+              stroke={"green"}
+              strokeWidth={0.5}
+              points={[100, 100, 300, 100]}
+            />
+            <Text x={350} y={90} text={"strokeWidth :0.5"} fontSize={20} />
 
-          <Line
-            x={10}
-            stroke={"green"}
-            strokeWidth={0.5}
-            points={[100, 100, 300, 100]}
-          />
-          <Text x={350} y={90} text={"strokeWidth :0.5"} fontSize={20} />
+            <Line
+              x={10}
+              stroke={"green"}
+              strokeWidth={0.1}
+              points={[100, 130, 300, 130]}
+            />
+            <Text x={350} y={120} text={"strokeWidth :0.1"} fontSize={20} />
 
-          <Line
-            x={10}
-            stroke={"green"}
-            strokeWidth={0.1}
-            points={[100, 130, 300, 130]}
-          />
-          <Text x={350} y={120} text={"strokeWidth :0.1"} fontSize={20} />
-
-          <Line
-            x={10}
-            stroke={"green"}
-            strokeWidth={0.01}
-            points={[100, 160, 300, 160]}
-          />
-          <Text x={350} y={150} text={"strokeWidth :0.01"} fontSize={20} />
+            <Line
+              x={10}
+              stroke={"green"}
+              strokeWidth={0.01}
+              points={[100, 160, 300, 160]}
+            />
+            <Text x={350} y={150} text={"strokeWidth :0.01"} fontSize={20} />
+          </Group>
         </Layer>
       </Stage>
     </>
